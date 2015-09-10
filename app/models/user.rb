39d8a22ecc0ed831124511399188dd3d7b47f35b
Mitchell Bank <mitchbank26@gmail.com>
2015-09-10 19:20:48 -0400

    Generated user controller, views, validations and error messages. Next time, I'll remember to commit more frequently

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
-class User < ActiveRecord::Base
-  has_many :reservations
-  has_many :restaurants, through: :reservations
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email. Bro, do you even type?")
+    end
+  end
+end
 
-  has_many :owned_restaurants, class_name: "Restaurant", foreign_key: "owner_id"
+class User < ActiveRecord::Base
+	
+	validates :first_name, :last_name, presence: true
+	validates :email, presence: true, email: true
+
+	has_secure_password
+
+	has_many :reservations
+	has_many :restaurants, through: :reservations
+
+	has_many :owned_restaurants, class_name: "Restaurant", foreign_key: "owner_id"
 end
