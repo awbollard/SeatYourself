51c85a18b45c7aacc13f1009d703720d6f055194
Mitchell Bank <mitchbank26@gmail.com>
2015-09-10 19:51:20 -0400

    Cleaned up code

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ end
 class User < ActiveRecord::Base
 	
 	validates :first_name, :last_name, presence: true
-	validates :email, presence: true, email: true
+	validates :email, email: true
 
 	has_secure_password
 
