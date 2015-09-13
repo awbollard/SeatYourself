4f9a1a24e2f73b05a33bd7fbfd5e07821a5f8e77
Mitchell Bank <mitchbank26@gmail.com>
2015-09-13 17:57:54 -0400

    Trying to get the restaurants 'show' view to display the reservation form. It's not working...

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,22 +3,22 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
-  def show
-  	@restaurant = Restaurant.find_by(id: params[:id])
-  end
-
-  def edit
-  	@restaurant = Restaurant.find_by(id: params[:id])
-  end
-
   def create
-  	@restaurant = Restaurant.new(restaurant_params)
+  	@restaurant = Restaurant.create(restaurant_params)
 
   	if @restaurant.save
-  		redirect_to restaurants_path
+  		redirect_to restaurant_path(@restaurant)
   	else
   		render :new
   	end
+  end
+
+  def show
+    @restaurant = Restaurant.find_by(id: params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find_by(id: params[:id])
   end
 
   def update
