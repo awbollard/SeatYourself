71e09aa8c4e45887a17e66839f9ce1aef83febd0
Jack Burum <jtburum@gmail.com>
2015-09-14 19:16:46 -0400

    Added reservation capacity functionality

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,7 @@ class Reservation < ActiveRecord::Base
 
 	validate :open_hours
 	validate :max_party_size, on: :partysize
+	validate :capacity
 
 	belongs_to :user
 	belongs_to :restaurant
@@ -15,6 +16,13 @@ class Reservation < ActiveRecord::Base
 	def max_party_size
 		unless partysize <= 6 && partysize >= 1
 			errors.add(:partysize, "We can't accomodate that party size, if you need to make a reservation for more than 6 people, please call us.")
+		end
+	end
+
+	def capacity
+		r = Reservation.where(dinner_time: dinner_time, restaurant_id: restaurant_id).sum(:partysize) 
+		unless (restaurant.capacity - r) >= partysize
+			errors.add(:partysize, "This restaurant is at capacity for this time. We suggest trying another time")
 		end
 	end
 end
